d83406ba7145135188c5bba1e26800664743b577
nikhil-shukla <nikhil1252000.shukla@gmail.com>
2024-10-16 14:09:15 +0530

    new commit

--- src/main/java/com/security/security/controllers/userController.java
+++ src/main/java/com/security/security/controllers/userController.java
@@ -43,8 +43,6 @@ public class userController {
     public Integer getInt(@RequestBody UniversalRequestHandler universalRequestHandler){
         List<User> myList = universalRequestHandler.getListValue("myList", User.class);
         return universalRequestHandler.getIntegerValue("myInt");
-
-        
     }
 
 }
